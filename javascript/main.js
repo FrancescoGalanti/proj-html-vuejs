4de29f1e65e26f6c22a32826be67509de2aec6bf
FrancescoGalanti <galanti_francesco>
2020-11-30 16:50:23 +0100

    working

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,29 @@ var app = new Vue({
     el: '#app',
     data: {
     // List of array that contains the link of navbar
-    listContact:["home","about","departments", "articles","make appointment"]
+    listContact:[
+        {
+          name: "home",
+          class: "Home-style"
+        },
+
+        {
+          name: "about"
+        },
+
+        {
+          name: "departments"
+        },
+
+        {
+          name: "articles"
+        },
+
+        {
+          name: "make an appointment",
+          class: "btn"
+        }
+     ],
    },
 
 
