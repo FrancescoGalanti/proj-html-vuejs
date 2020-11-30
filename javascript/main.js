99161bb77bfdce34ef2def8ef894888852cd887b
FrancescoGalanti <galanti_francesco>
2020-11-30 14:45:27 +0100

    nav-left

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+    // List of array that contains the link of navbar
+    listContact:["home","about","departments", "articles","make appointment"]
    },
 
 
