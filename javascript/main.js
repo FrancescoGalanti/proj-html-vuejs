e0ed18f6343d6b1df6da348c0c2da3335295125f
FrancescoGalanti <galanti_francesco>
2020-12-02 23:14:53 +0100

    fixing

--- javascript/main.js
+++ javascript/main.js
@@ -31,36 +31,17 @@ var app = new Vue({
 
      // List user
      listUser:[
-       { name: "",
-         email: "",
-         Phone: "",
-         Date: "",
-         help: "",
+       {
+
 
        },
-       { name: "",
-         email: "",
-         Phone: "",
-         Date: "",
-         help: "",
-
-       },
-       { name: "",
-         email: "",
-         Phone: "",
-         Date: "",
-         help: "",
-
-       }
      ],
-
+     // strings of vlaue managed by an input
      newName: "",
 
      newEmail: "",
 
      newPhone: "",
-
-     newDate: "",
 
      newDate: "",
 
@@ -72,26 +53,31 @@ var app = new Vue({
    },
 
    methods: {
-      // Call back function that use the arrays index and the controll variable to match the user with the same message inside the array
-      Setactivecontact(index) {
-       // comparing the control variable to the array's index//
-         this.indexId = index;
 
-      },
 
       // callback function that use the add the new strings in the array
       addingNewAppointment(){
-        if((this.newName.trim() !== "") && (this.newEmail.trim() !== "") &&()){
-          // array mached with the control variable to push the newchat in the esact contact.name and messages//
-          this.listUser[this.indexId].messages.push({
-                 // Format date made by Data,js //
-                 date: dayjs().format('DD/MM/YY HH:mm:ss'),
-                 // Create a new messge.value with previously the function previously described //
-                 message: this.newchat.trim(),
-                 // assignment of the object value to assign the right class in css //
-                 status: 'sent',
-
+          // validation
+          if((this.newName.trim() !== "") && (this.newName.trim() !== "")  && (this.newName.trim() !== "") && (this.newName.trim() !== "") && (this.newName.trim() !== "")){
+             this.listUser.push({
+               name:  this.newName,
+               email: this.newEmail,
+               phone: this.newPhone,
+               date: this.newDate,
+               help: this.newHelp
              });
+
+             console.log(this.listUser)
+
+            // deafukt situaation
+
+             this.newName = "";
+             this.newEmail ="";
+             this.newPhone = "";
+             this.newDate = "";
+             this.newHelp = "";
+
+          }
       }
 
     }
