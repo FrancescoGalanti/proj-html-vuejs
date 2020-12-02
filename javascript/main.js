9c5dd09bf310291613e07c55021e2631716d989f
FrancescoGalanti <galanti_francesco>
2020-12-02 16:05:03 +0100

    working with vue in section appointment

--- javascript/main.js
+++ javascript/main.js
@@ -28,19 +28,73 @@ var app = new Vue({
           class: "btn"
         }
      ],
+
+     // List user
+     listUser:[
+       { name: "",
+         email: "",
+         Phone: "",
+         Date: "",
+         help: "",
+
+       },
+       { name: "",
+         email: "",
+         Phone: "",
+         Date: "",
+         help: "",
+
+       },
+       { name: "",
+         email: "",
+         Phone: "",
+         Date: "",
+         help: "",
+
+       }
+     ],
+
+     newName: "",
+
+     newEmail: "",
+
+     newPhone: "",
+
+     newDate: "",
+
+     newDate: "",
+
+     newHelp: "",
+
+
+
+
    },
 
-   newName: "",
+   methods: {
+      // Call back function that use the arrays index and the controll variable to match the user with the same message inside the array
+      Setactivecontact(index) {
+       // comparing the control variable to the array's index//
+         this.indexId = index;
 
-   newEmail: "",
+      },
 
-   newPhone: "",
+      // callback function that use the add the new strings in the array
+      addingNewAppointment(){
+        if((this.newName.trim() !== "") && (this.newEmail.trim() !== "") &&()){
+          // array mached with the control variable to push the newchat in the esact contact.name and messages//
+          this.listUser[this.indexId].messages.push({
+                 // Format date made by Data,js //
+                 date: dayjs().format('DD/MM/YY HH:mm:ss'),
+                 // Create a new messge.value with previously the function previously described //
+                 message: this.newchat.trim(),
+                 // assignment of the object value to assign the right class in css //
+                 status: 'sent',
 
-   newDate: "",
+             });
+      }
 
-   newDate: "",
-
-   newHelp: "",
+    }
 
 
   });
